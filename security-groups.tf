d2fe69da61ded0aaf657edb4462a1838f7b38ca7
rstm <rstm@gmail.com>
2022-12-04 18:16:04 +0300

    Opsfleet cluster v1

--- security-groups.tf
+++ security-groups.tf
@@ -1,29 +1,105 @@
+# Node Group One
+
 resource "aws_security_group" "node_group_one" {
   name_prefix = "node_group_one"
   vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+}
 
-    cidr_blocks = [
+resource "aws_security_group_rule" "node_group_one_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = [
       "10.0.0.0/8",
     ]
-  }
+
+  security_group_id = aws_security_group.node_group_one.id
 }
+
+resource "aws_security_group_rule" "node_group_one_in_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = [
+      "10.0.0.0/8",
+    ]
+  security_group_id = aws_security_group.node_group_one.id
+}
+
+resource "aws_security_group_rule" "node_group_one_in_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks = [
+      "10.0.0.0/8",
+    ]
+  security_group_id = aws_security_group.node_group_one.id
+}
+
+resource "aws_security_group_rule" "node_group_one_in_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks = [
+      "10.0.0.0/8",
+    ]
+  security_group_id = aws_security_group.node_group_one.id
+}
+
+# Node Group Two
 
 resource "aws_security_group" "node_group_two" {
   name_prefix = "node_group_two"
   vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+}
 
-    cidr_blocks = [
+resource "aws_security_group_rule" "node_group_two_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = [
       "192.168.0.0/16",
     ]
-  }
+
+  security_group_id = aws_security_group.node_group_two.id
+}
+
+resource "aws_security_group_rule" "node_group_two_in_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = [
+      "192.168.0.0/16",
+    ]
+  security_group_id = aws_security_group.node_group_two.id
+}
+
+resource "aws_security_group_rule" "node_group_two_in_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks = [
+      "192.168.0.0/16",
+    ]
+  security_group_id = aws_security_group.node_group_two.id
+}
+
+resource "aws_security_group_rule" "node_group_two_in_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks = [
+      "192.168.0.0/16",
+    ]
+  security_group_id = aws_security_group.node_group_two.id
 }
